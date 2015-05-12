d0cdc07c243cb85842d3e6e671ff131dd8e145ed
Simon Layfield <simonlayfield@gmail.com>
2015-05-12 10:11:59 +1000

    Added subtractions to value selections

--- mp3/js/app-scripts.js
+++ mp3/js/app-scripts.js
@@ -1,34 +1,50 @@
-
+	
 $(document).ready(function(){
 
-
+	
 	function updateAppTotal(appTotal){
-
+	
 		$('.appTotal h3').text(appTotal);
-
+	
 	}
 
 
 	// Calculate App Totals
-
-		var appTotal = 0;
-
-		$('.value').on('click', function(){
-
-			var clickedOption = $(this);
-
-			if((clickedOption).hasClass('value')){
-
-				var clickedOptionValue = parseInt(clickedOption.attr('data-val'));
-
-				appTotal += clickedOptionValue;
-
-				updateAppTotal(appTotal);
-
-			}
-
-		});
-
-
-
+		
+	var appTotal = 0;
+	
+	$('.value').on('click', function(){
+	
+		// Check if any other element within the view is selected
+		
+		var currentlySelected = $('#view').find('.selected');
+		
+		if (currentlySelected != ""){
+			
+			selectedValue = parseInt(currentlySelected.attr('data-val'));
+			
+			appTotal -= selectedValue;
+			
+			updateAppTotal(appTotal);
+			
+		}
+	
+		var clickedOption = $(this);
+		
+		if((clickedOption).hasClass('value')){
+		
+			var clickedOptionValue = parseInt(clickedOption.attr('data-val'));
+			
+			appTotal += clickedOptionValue;
+			
+			clickedOption.addClass('selected');
+			
+			updateAppTotal(appTotal);
+		
+		}
+	
+	});
+		
+		
+		
 });

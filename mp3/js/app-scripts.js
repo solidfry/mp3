a2be6dc08deafe68065ba98512542614ecb031cb
solidfry <ellioteweb@gmail.com>
2015-06-01 10:38:35 +1000

    added updated device button

--- mp3/js/app-scripts.js
+++ mp3/js/app-scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+    var updateDevice = $('.model__config-panel .button');
+    var updateAnim = '<div class="updated"><span class="checkmarkWrap"><span class="checkmark"><div class="checkmark_circle"></div><div class="checkmark_stem"></div><div class="checkmark_kick"></div></span></span></div>';
+    updateDevice.click(function() {
+        $this.parent().find(".model__config-panel").prepend(updateAnim);
+    });
+
 	$('.viewport').css('width', $(window).width());
 
     /*$('.home .model__item').each(function() {
